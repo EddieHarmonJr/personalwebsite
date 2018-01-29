064d9123a91cc91cdaf5e8ce56bab51540a0a8b1
eddieharmonjr <eddieharmon.ac.c4q.nyc>
2018-01-29 15:15:04 -0500

    add home, about me, resume nav bar links.

--- personalwebsite/src/App.js
+++ personalwebsite/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Link, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Link to='/'>HOME</Link>{" "}
+        <Link to='/aboutme'>ABOUT ME</Link>{" "}
+        <Link to='/resume'>RESUME</Link>{" "}
         <h1>Hello World!</h1>
+        <Switch>
+          {/*<Route exact path="/aboutme" component={AboutMe} />*/}
+          {/*<Route exact path="/aboutme" component={Resume} />*/}
+        </Switch>
       </div>
     );
   }
